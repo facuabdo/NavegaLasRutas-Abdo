1c6e9a4b520a466c59f7770a48953b63d50650cb
Facundo Abdo <facundo.abdo@globant.com>
2023-11-13 00:06:06 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+import logo from "./assets/facommerce-logo.png";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={logo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>Facommerce</h1>
+    </>
+  );
+}
+
+export default App;
