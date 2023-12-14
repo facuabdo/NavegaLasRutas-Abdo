13007d147381186adf7aa8fec78679a34fa8184b
Facundo Abdo <facundo.abdo@globant.com>
2023-12-14 02:14:18 -0300

    obtiene productos del mock

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,5 +1,28 @@
 /* eslint-disable react/prop-types */
 
+import { useEffect, useState } from "react";
+
+import { getProductos } from "../../data/dataMock";
+
 export const ItemListContainer = ({ title }) => {
-  return <div className="item-list-container">{title}</div>;
+  const [productos, setProductos] = useState(null);
+
+  useEffect(() => {
+    getProductos().then((productosList) => setProductos(productosList));
+  }, []);
+
+  return (
+    <div className="item-list-container">
+      {title}
+      <hr />
+
+      {productos
+        ? productos.length > 0
+          ? productos.map((producto) => (
+              <h1 key={producto.id}>{producto.nombre}</h1>
+            ))
+          : "No hay productos para mostrar"
+        : "Cargando productos..."}
+    </div>
+  );
 };
