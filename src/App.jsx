4e39a4e688f2c7d8f48c84751a6fefa87acfddc5
Facundo Abdo <facundo.abdo@globant.com>
2023-12-14 01:07:15 -0300

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import "./App.css";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import { NavBar } from "./components/NavBar/NavBar";
 
 function App() {
   return (
-    <div className="container">
-      <NavBar />
-      <ItemListContainer title="aca irian los items" />
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <NavBar />
+        <Routes>
+          <Route
+            path="/"
+            element={<ItemListContainer title="aca irian los items" />}
+          />
+          <Route path="*" element={<h1>404</h1>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
